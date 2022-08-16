5247e800bd2dd9a3de9a649bb068d35ec94d75ad
wivw0306 <wivw0306@163.com>
2022-08-16 23:28:41 +0800

    update file

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -18,6 +18,7 @@
 <script>
 import RightPanel from '@/components/RightPanel' // 这个是设置的icon组件
 import { AppMain, Navbar, Settings, Sidebar, TagsView } from './components'
+import ResizeMixin from './mixin/ResizeHandler'
 import { mapState } from 'vuex'
 
 export default {
@@ -30,6 +31,7 @@ export default {
     Sidebar,
     TagsView
   },
+  mixins: [ResizeMixin],
   computed: {
     ...mapState({
       sidebar: state => state.app.sidebar,
@@ -52,7 +54,49 @@ export default {
       this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
     }
   }
-
 }
 </script>
 
+<style lang="scss" scoped>
+  @import "~@/styles/mixin.scss";
+  @import "~@/styles/variables.scss";
+
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+
+    &.mobile.openSidebar {
+      position: fixed;
+      top: 0;
+    }
+  }
+
+  .drawer-bg {
+    background: #000;
+    opacity: 0.3;
+    width: 100%;
+    top: 0;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+  }
+
+  .fixed-header {
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 9;
+    width: calc(100% - #{$sideBarWidth});
+    transition: width 0.28s;
+  }
+
+  .hideSidebar .fixed-header {
+    width: calc(100% - 54px)
+  }
+
+  .mobile .fixed-header {
+    width: 100%;
+  }
+</style>
